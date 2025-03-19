052649e94b112cd07f7fdd75b5bc639431735adb
colinskorir <korircoll20@gmail.com>
2025-03-19 17:52:11 +0300

    files added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value;
+
+    if (taskText.trim() !== "") {
+  
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+
+      taskList.appendChild(taskItem);
+
+      taskInput.value = "";
+    }
+  });
 });
